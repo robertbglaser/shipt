7bf263b22a852d8f199d4218e67e88ab2b3dd9b3
Robert Glaser <robertbglaser@gmail.com>
2018-10-21 14:55:24 -0700

    add to new branch

--- public/shipt.js
+++ public/shipt.js
@@ -5,16 +5,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let sign = window.prompt('Please enter your guess.  A-Z 1-9');
   console.log("the imput was " + sign);
 
-  (function(){
-  var dialog = ('<p>Are you sure?</p>').dialog({
-    buttons: {
-        "Yes": function() {alert('you chose yes');},
-        "No":  function() {alert('you chose no');},
-        "Cancel":  function() {
-            alert('you chose cancel');
-            dialog.dialog('close');
-        }
-    }
-})
   
-})();
