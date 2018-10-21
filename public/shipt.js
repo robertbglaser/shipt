c0636cdb1c9c6d081dffee0aac88a059fcd540d7
Robert Glaser <robertbglaser@gmail.com>
2018-10-21 15:11:33 -0700

    add grid

--- public/shipt.js
+++ public/shipt.js
@@ -5,4 +5,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let sign = window.prompt('Please enter your guess.  A-Z 1-9');
   console.log("the imput was " + sign);
 
+    var compGrid = {status: " ", Grid1:"a4", Grid2:"a5", Grid3:"a6"};  
+
   
