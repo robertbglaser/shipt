0ad0514dc5a33cd8b94be3f3e38157a759a49802
Robert Glaser <robertbglaser@gmail.com>
2018-10-22 17:47:05 -0700

    add more code

--- public/shipt.js
+++ public/shipt.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
    
   });
   let humanInput = " ";
-  let humanGrid = [];
-  let humanPlacement = {
+  let humanGridInput = [];
+  var humanGrid = {
     vesselLength: 0,
     vesselPlacement:{
         cell: '',
-        cell1Status: ''
+        cellStatus: ''
     }
 }
     
@@ -15,22 +15,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
   while(humanInput !== null){
         humanInput = window.prompt('Please enter the coordinates of your ships seperated by commas.  A-Z 1-9');
         console.log("the input was " + humanInput);
-        humanGrid = humanInput.split(', ')
-        console.log(humanGrid)
+        humanGridInput = humanInput.split(', ')
+        console.log()
         humanInput = window.prompt('Your response has been recorded, press cancel to end the input process');
   }
  console.log("outside of the while")
 
-   humanGrid.forEach (function(item, index,humanGrid){
-       console.log("In the forEach " + item, index)
-      var cellNum = index +1;
-      console.log("length= " + cellNum);
-      humanPlacement.vesselLength.push(cellNum);
-      humanPlacement.status.push("");
-      humanPlacement.cell.push(item);
+   humanGrid = humanGridInput.map(function(value){
+   // var cellNum = index +1;
+    console.log("the value is " + value); 
+    return value;
    });
+
+   
+
+ /*  pluralArray = singularArray.map(function(value){
+    return value+'s';
+ 
+ humanGridInput.forEach (function(item, index, humanGridInput){
+      var cellNum = index +1;
+      humanGrid.vesselLength = cellNum;
+      console.log(index + " "+item);
+    //  humanGrid.vesselPlacement[index].status = "blank";
+      humanGrid.vesselPlacement[index].cell = item;
+   }); */
   
-      
+       console.log("after forEach" + JSON.parse(humanGrid));
   
 
     var compGrid = {status: " ", Grid1:"a4", Grid2:"a5", Grid3:"a6"};  
