1da64624a42cae82e1a9841bb5dccfcc3533acb3
Robert Glaser <robertbglaser@gmail.com>
2018-10-24 08:08:41 -0700

    added loadhuman function

--- public/shipt.js
+++ public/shipt.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
   var gamePrompt = window.prompt('Want to play a game?');
   
-      console.log(gamePrompt)
+      console.log(gamePrompt);
 
   
   let humanInput = " ";
-  let humanGridInput = [];
+  let userInput = [];
   var humanGrid = {
     vesselLength: 0,
     vesselPlacement:{
@@ -20,44 +20,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
   while(humanInput !== null){
         humanInput = window.prompt('Please enter the coordinates of your ships seperated by commas.  A-Z 1-9');
         console.log("the input was " + humanInput);
-        humanGridInput = humanInput.split(', ')
-        console.log()
-        humanInput = window.prompt('Your response has been recorded, press cancel to end the input process');
+        if (humanInput !== null) {
+            userInput = humanInput.split(', ')
+            loadHumanGrid();
+            console.log("****** "+JSON.stringify(humanGrid));
+            humanInput = window.prompt('Your response has been recorded, press cancel to end the input process');
+        } else {
+              var z = window.prompt('Thank you for playing the game' );
+        }
+        
   }
- console.log("outside of the while")
-   var cellNum = 0;
-   humanGrid = humanGridInput.map(function(value){
-   cellNum += 1
-
-   humanGrid.vesselLength.push(cellNum);
-   humanGrid.vesselPlacement.cell.push(humanGridInput)
-    console.log("the value is " + humanGrid.vesselPlacement.cell); 
-    return value;
-   });
-
-   //for 
-
-   
-
- /*  pluralArray = singularArray.map(function(value){
-    return value+'s';
  
- humanGridInput.forEach (function(item, index, humanGridInput){
-      var cellNum = index +1;
-      humanGrid.vesselLength = cellNum;
-      console.log(index + " "+item);
-    //  humanGrid.vesselPlacement[index].status = "blank";
-      humanGrid.vesselPlacement[index].cell = item;
-   }); */
-  
-       console.log("after the map " + JSON.stringify(humanGrid.vesselPlacement) );
+ 
+ function loadHumanGrid(){
+     console.log("inside loadHumanGrid");
+     userInput.forEach (function(item, index, humanGridInput){
+        console.log(item +  " " + index); 
+        var cellNum = index +1;
+        return {
+            vesselLength: cellNum,
+            status: " ",
+            cell: item
+        }
+     });  
+ }
+    
+       
 
     
-        console.log("play the game here");
-        humanInput = window.prompt('');
+      /*  console.log("play the game here");
+        humanInput = window.prompt(''); */
     
   
 
-    var compGrid = {status: " ", Grid1:"a4", Grid2:"a5", Grid3:"a6"};  
+  //  var compGrid = {status: " ", Grid1:"a4", Grid2:"a5", Grid3:"a6"};  
 
   
